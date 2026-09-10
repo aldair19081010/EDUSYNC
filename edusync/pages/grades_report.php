7c7b69b287d32c037b28f9967817e0fd223ef07c
aldair19081010 <jazz02981@gmail.com>
2026-09-10 08:07:50 -0500

    Actualizar versión del script de reporte de notas

--- edusync/pages/grades_report.php
+++ edusync/pages/grades_report.php
@@ -2,4 +2,4 @@
 // The report core verifies director access without changing the session role.
 require __DIR__ . '/grades_report_core.php';
 ?>
-<script src="js/grades_report_excel_export.js?v=20260910"></script>
+<script src="js/grades_report_excel_export.js?v=20260910-2"></script>
