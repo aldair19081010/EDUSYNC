f6313e9324f4664e452ccc071cc1a690ae70c71d
aldair19081010 <jazz02981@gmail.com>
2026-09-11 18:33:29 -0500

    Cargar capa global de consistencia visual

--- edusync/includes/navbar.php
+++ edusync/includes/navbar.php
@@ -1,17 +1,27 @@
 <?php
 require __DIR__ . '/navbar_view.php';
 
-$ui_css_file = dirname(__DIR__) . '/css/custom.css';
+$custom_css_file = dirname(__DIR__) . '/css/custom.css';
+$ui_css_file = dirname(__DIR__) . '/css/ui_consistency.css';
 $ui_js_file = dirname(__DIR__) . '/js/ui_consistency.js';
+$custom_css_version = @filemtime($custom_css_file) ?: time();
 $ui_css_version = @filemtime($ui_css_file) ?: time();
 $ui_js_version = @filemtime($ui_js_file) ?: time();
 ?>
 <script>
 (function(){
-    var link = document.querySelector('link[href^="css/custom.css"]');
-    if (link) {
-        var base = (link.getAttribute('href') || 'css/custom.css').split('?')[0];
-        link.setAttribute('href', base + '?v=<?php echo rawurlencode((string)$ui_css_version); ?>');
+    var custom = document.querySelector('link[href^="css/custom.css"]');
+    if (custom) {
+        var customBase = (custom.getAttribute('href') || 'css/custom.css').split('?')[0];
+        custom.setAttribute('href', customBase + '?v=<?php echo rawurlencode((string)$custom_css_version); ?>');
+    }
+
+    if (!document.querySelector('link[data-edusync-ui-consistency]')) {
+        var ui = document.createElement('link');
+        ui.rel = 'stylesheet';
+        ui.href = 'css/ui_consistency.css?v=<?php echo rawurlencode((string)$ui_css_version); ?>';
+        ui.setAttribute('data-edusync-ui-consistency', '1');
+        document.head.appendChild(ui);
     }
 })();
 </script>
