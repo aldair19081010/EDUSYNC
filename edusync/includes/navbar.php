db4f385d224c170cd9a6426ace9e36348591ba53
aldair19081010 <jazz02981@gmail.com>
2026-09-11 19:07:03 -0500

    Cargar ajuste de tablas del reporte de notas

--- edusync/includes/navbar.php
+++ edusync/includes/navbar.php
@@ -4,10 +4,12 @@ require __DIR__ . '/navbar_view.php';
 $custom_css_file = dirname(__DIR__) . '/css/custom.css';
 $ui_css_file = dirname(__DIR__) . '/css/ui_consistency.css';
 $ui_refinements_file = dirname(__DIR__) . '/css/ui_refinements.css';
+$ui_report_tables_file = dirname(__DIR__) . '/css/ui_report_tables.css';
 $ui_js_file = dirname(__DIR__) . '/js/ui_consistency.js';
 $custom_css_version = @filemtime($custom_css_file) ?: time();
 $ui_css_version = @filemtime($ui_css_file) ?: time();
 $ui_refinements_version = @filemtime($ui_refinements_file) ?: time();
+$ui_report_tables_version = @filemtime($ui_report_tables_file) ?: time();
 $ui_js_version = @filemtime($ui_js_file) ?: time();
 ?>
 <script>
@@ -32,6 +34,14 @@ $ui_js_version = @filemtime($ui_js_file) ?: time();
         refinements.href = 'css/ui_refinements.css?v=<?php echo rawurlencode((string)$ui_refinements_version); ?>';
         refinements.setAttribute('data-edusync-ui-refinements', '1');
         document.head.appendChild(refinements);
+    }
+
+    if (!document.querySelector('link[data-edusync-ui-report-tables]')) {
+        var reportTables = document.createElement('link');
+        reportTables.rel = 'stylesheet';
+        reportTables.href = 'css/ui_report_tables.css?v=<?php echo rawurlencode((string)$ui_report_tables_version); ?>';
+        reportTables.setAttribute('data-edusync-ui-report-tables', '1');
+        document.head.appendChild(reportTables);
     }
 })();
 </script>
