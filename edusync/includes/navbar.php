4271fc7f1d1a2c6801809a6320358c85e1f81227
aldair19081010 <jazz02981@gmail.com>
2026-09-11 19:00:30 -0500

    Cargar refinamientos visuales globales

--- edusync/includes/navbar.php
+++ edusync/includes/navbar.php
@@ -3,9 +3,11 @@ require __DIR__ . '/navbar_view.php';
 
 $custom_css_file = dirname(__DIR__) . '/css/custom.css';
 $ui_css_file = dirname(__DIR__) . '/css/ui_consistency.css';
+$ui_refinements_file = dirname(__DIR__) . '/css/ui_refinements.css';
 $ui_js_file = dirname(__DIR__) . '/js/ui_consistency.js';
 $custom_css_version = @filemtime($custom_css_file) ?: time();
 $ui_css_version = @filemtime($ui_css_file) ?: time();
+$ui_refinements_version = @filemtime($ui_refinements_file) ?: time();
 $ui_js_version = @filemtime($ui_js_file) ?: time();
 ?>
 <script>
@@ -22,6 +24,14 @@ $ui_js_version = @filemtime($ui_js_file) ?: time();
         ui.href = 'css/ui_consistency.css?v=<?php echo rawurlencode((string)$ui_css_version); ?>';
         ui.setAttribute('data-edusync-ui-consistency', '1');
         document.head.appendChild(ui);
+    }
+
+    if (!document.querySelector('link[data-edusync-ui-refinements]')) {
+        var refinements = document.createElement('link');
+        refinements.rel = 'stylesheet';
+        refinements.href = 'css/ui_refinements.css?v=<?php echo rawurlencode((string)$ui_refinements_version); ?>';
+        refinements.setAttribute('data-edusync-ui-refinements', '1');
+        document.head.appendChild(refinements);
     }
 })();
 </script>
