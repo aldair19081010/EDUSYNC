409630ae51de5e4b1d94cb060913da238f2e994b
aldair19081010 <jazz02981@gmail.com>
2026-09-11 19:13:46 -0500

    Cargar ajustes de ficha individual de notas

--- edusync/includes/navbar.php
+++ edusync/includes/navbar.php
@@ -5,11 +5,13 @@ $custom_css_file = dirname(__DIR__) . '/css/custom.css';
 $ui_css_file = dirname(__DIR__) . '/css/ui_consistency.css';
 $ui_refinements_file = dirname(__DIR__) . '/css/ui_refinements.css';
 $ui_report_tables_file = dirname(__DIR__) . '/css/ui_report_tables.css';
+$ui_grades_individual_file = dirname(__DIR__) . '/css/ui_grades_individual.css';
 $ui_js_file = dirname(__DIR__) . '/js/ui_consistency.js';
 $custom_css_version = @filemtime($custom_css_file) ?: time();
 $ui_css_version = @filemtime($ui_css_file) ?: time();
 $ui_refinements_version = @filemtime($ui_refinements_file) ?: time();
 $ui_report_tables_version = @filemtime($ui_report_tables_file) ?: time();
+$ui_grades_individual_version = @filemtime($ui_grades_individual_file) ?: time();
 $ui_js_version = @filemtime($ui_js_file) ?: time();
 ?>
 <script>
@@ -42,6 +44,14 @@ $ui_js_version = @filemtime($ui_js_file) ?: time();
         reportTables.href = 'css/ui_report_tables.css?v=<?php echo rawurlencode((string)$ui_report_tables_version); ?>';
         reportTables.setAttribute('data-edusync-ui-report-tables', '1');
         document.head.appendChild(reportTables);
+    }
+
+    if (!document.querySelector('link[data-edusync-ui-grades-individual]')) {
+        var gradesIndividual = document.createElement('link');
+        gradesIndividual.rel = 'stylesheet';
+        gradesIndividual.href = 'css/ui_grades_individual.css?v=<?php echo rawurlencode((string)$ui_grades_individual_version); ?>';
+        gradesIndividual.setAttribute('data-edusync-ui-grades-individual', '1');
+        document.head.appendChild(gradesIndividual);
     }
 })();
 </script>
